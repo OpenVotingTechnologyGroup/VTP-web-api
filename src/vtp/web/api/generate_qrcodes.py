e727f3bba9983f1f2a5c6bb6b6c90ae42b829098
Sandy Currier <windoverwater@users.noreply.github.com>
2024-05-14 16:58:20 -0400

    a better QRcode generator

--- src/vtp/web/api/generate_qrcodes.py
+++ src/vtp/web/api/generate_qrcodes.py
@@ -6,18 +6,6 @@ import socket
 import requests
 import segno
 
-# Get the external IP
-try:
-    response = requests.get("http://ip.jsontest.com/", timeout=5)
-except requests.exceptions.Timeout:
-    print("Note - http://ip.jsontest.com/ timed out")
-    WAN_IP = "0.0.0.0"
-if response.status_code == 200:
-    WAN_IP = response.json()["ip"]
-else:
-    print("Note - did not receive 200 status code")
-    WAN_IP = "0.0.0.0"
-
 
 # Get the LAN (or localhost) IP
 def get_local_ip():
@@ -36,13 +24,31 @@ def get_local_ip():
     return this_ip
 
 
+def get_wan_ip():
+    """a persnickity get the external IP"""
+    try:
+        response = requests.get("http://ip.jsontest.com/", timeout=5)
+        if response.status_code == 200:
+            that_ip = response.json()["ip"]
+        else:
+            print("Note - did not receive 200 status code")
+            that_ip = "0.0.0.0"
+    except requests.exceptions.Timeout:
+        print("Note - http://ip.jsontest.com/ timed out")
+        that_ip = "0.0.0.0"
+    except:  # pylint: disable=bare-except
+        print("Note - could not effectively get to the internet")
+        that_ip = "0.0.0.0"
+    return that_ip
+
+
 LAN_IP = get_local_ip()
-
 print(f"LAN_IP={LAN_IP}")
-print(f"WAN_IP={WAN_IP}")
-
-wan_qrcode = segno.make_qr("http://" + WAN_IP + ":8000")
 lan_qrcode = segno.make_qr("http://" + LAN_IP + ":8000")
-
-wan_qrcode.save(WAN_IP + ".svg", scale=5, light="#EFF1C5", dark="darkgreen")
 lan_qrcode.save(LAN_IP + ".svg", scale=5, light="#EFF1C5", dark="darkgreen")
+
+WAN_IP = get_wan_ip()
+if WAN_IP != "0.0.0.0":
+    print(f"WAN_IP={WAN_IP}")
+    wan_qrcode = segno.make_qr("http://" + WAN_IP + ":8000")
+    wan_qrcode.save(WAN_IP + ".svg", scale=5, light="#EFF1C5", dark="darkgreen")
